e356077a8a43ecd6e628ad69a906f6d7cf56c6f7
Gintare <gintaar3@gmail.com>
2020-09-02 14:25:09 +0200

    counting function

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 "use strict";
 
-// TO:DO
-// Make counting function - maybe setTimeout or setInterval
-// Find how to push to beinning and remove from end - maybe if
+window.addEventListener("load", startCounter); //Start counter on load
+
+let number = 0;
+const array = [];
+
+function startCounter() {
+  //Set interval to repeat +1 to the conter every second
+  setInterval(function () {
+    array.unshift(number); //Push to beginning of array
+    number += 1;
+    console.log(array);
+
+    //Check lenght and remove last item
+    if (array.length >= 9) {
+      array.pop(); //Remove from end of array
+    }
+  }, 1000);
+}
